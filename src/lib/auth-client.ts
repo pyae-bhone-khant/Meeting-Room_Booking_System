19079d9fea84adf293108d7ba794b9fc3b45845d
pyae-bhone-khant <pyaebhonen4@gmail.com>
2026-06-02 23:32:49 +0630

    TEST

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,7 @@
-import { createAuthClient } from "better-auth/react";
+import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-  // .env.local ထဲက နာမည်အတိုင်း ကွက်တိဖြစ်သွားပါပြီ
-  baseURL: process.env.NEXT_PUBLIC_AUTH_URL || "http://localhost:3000", 
-});
+  baseURL: process.env.NEXT_PUBLIC_AUTH_URL || "http://localhost:3000",
+})
 
-export const { signIn, signUp, useSession } = authClient;
+export const { signIn, signUp, useSession } = authClient
