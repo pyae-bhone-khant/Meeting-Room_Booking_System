94453f123748cea59948c52fb5b89d0fec1cf07a
pyae-bhone-khant <pyaebhonen4@gmail.com>
2026-07-20 12:22:37 +0630

    latest commit

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
+  baseURL: "api/auth",
   fetchOptions: {
     credentials: "include",
   },
