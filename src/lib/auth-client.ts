ce9f448bb221d6a9e637531300fb5f99239688c3
pyae-bhone-khant <pyaebhonen4@gmail.com>
2026-07-20 13:01:51 +0630

    latest commit

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -9,11 +9,15 @@ import { createAuthClient } from "better-auth/react"
 // }) 
 
 // (Local တွင် http://localhost:3000 ဖြစ်ပြီး၊ Production Vercel တွင် မိမိ Vercel Domain ဖြစ်ပါမည်)
-const appUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+// const appUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
 export const authClient = createAuthClient({
   // အပြည့်အစုံဖြစ်သော URL (e.g., http://localhost:3000/api/auth) ကို ထည့်ပေးရပါမည်
-  baseURL: `${appUrl}/api/auth`, 
+  // baseURL: `${appUrl}/api/auth`,  
+   baseURL:
+    process.env.NEXT_PUBLIC_API_URL ?
+      `${process.env.NEXT_PUBLIC_API_URL}/api/auth`
+    : "http://localhost:8080/api/v1",
   fetchOptions: {
     credentials: "include",
   },
